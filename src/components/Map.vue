cb0cc291d13a069f795e7e6329e00a69fa2c961b
Pos-cmd <afolabiyasser06@gmail.com>
2023-04-07 14:44:05 +0000

    add map

---
+++ src/components/Map.vue
@@ -0,0 +1,17 @@
+<template>
+    <section class="map">
+        <div>
+            <iframe
+                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d10926.62769140608!2d-71.29485528071773!3d46.79136423117064!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4cb896e05475c2ff%3A0x9d356c46d4cce2b6!2s2323%20Rue%20Galvani%2C%20Qu%C3%A9bec%2C%20QC%20G1N%204H7%2C%20Canada!5e0!3m2!1sfr!2stg!4v1680875553742!5m2!1sfr!2stg"
+                loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+        </div>
+    </section>
+</template>
+
+<style scoped>
+iframe {
+    width: 100%;
+    height: 300px;
+    border: 0;
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,14 +1,17 @@
-
 <script setup>
 import PageHeader from '../components/Header.vue'
+import PageHero from '../components/Hero.vue'
 import Schedule from '../components/Schedule/Schedule.vue'
 import ContactForm from '../components/ContactForm.vue'
+import PageMap from '../components/Map.vue'
 import PageFooter from '../components/Footer.vue'
 </script>
 
 <template>
   <page-header />
+  <page-hero />
   <schedule />
   <contact-form />
+  <page-map />
   <page-footer />
 </template>
